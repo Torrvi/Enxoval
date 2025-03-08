b8187ff19d0fe933ffdb2672de31691c36406c73
Torrvi <Coelhozz24@gmail.com>
2025-03-08 11:59:44 -0300

    Ajustes

--- enxoval.js
+++ enxoval.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const botaoEntrar = document.getElementById("entrar");
+    if (botaoEntrar) {
+        botaoEntrar.addEventListener("click", entrar);
+    }
+});
 function entrar(){
-    const user = document.getElementById("nome")
+    console.log("O arquivo enxoval.js foi carregado!")
+    const usuario = document.getElementById("nome").value;
+    const senha = document.getElementById("senha").value;
+    const usuariocadastrado = ["Garota" , "Garoto"] ;
+    const senhacadastrada = "031224";
+    if(usuariocadastrado.includes(usuario)&& senha === senhacadastrada){
+        window.location.href = "produtos.html";
+    } else{
+        window.alert("Ainda não acertou, Garota");
+        document.getElementById("nome").value = "";
+        document.getElementById("senha").value = "";
+    }
 }
